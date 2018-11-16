eb2667270b8d9d5f74b648e0d773642dc8f5da69
wizbowes <wizbowes@gmail.com>
2018-11-16 00:26:45 +0000

    Update action-wizbowes-turn_on-wizbowes.HomeAssistant_via_REST_API.py

--- action-wizbowes-turn_on-wizbowes.HomeAssistant_via_REST_API.py
+++ action-wizbowes-turn_on-wizbowes.HomeAssistant_via_REST_API.py
@@ -41,17 +41,15 @@ def action_wrapper(hermes, intentMessage, conf):
         myDeviceName = intentMessage.slots.device_name.first().value
 # put this line back one once the bug is resolved: https://github.com/snipsco/snips-issues/issues/68
 #        myDeviceName = intentMessage.slots.device_name.first().raw_value 
-
+     header = {'x-ha-access': conf['secret']['haapipassword'], 'Content-Type': 'application/json'}
 
      if myState != "query":
        payload = json.dumps({"entity_id": myDeviceId})
        url = 'http://'+ conf['secret']['haipaddress'] + ':' + conf['secret']['haport'] + '/api/services/homeassistant/turn_' + myState
-       header = {'x-ha-access': conf['secret']['haapipassword'], 'Content-Type': 'application/json'}
        response = post(url, headers=header, data=payload)
        hermes.publish_end_session(current_session_id, "Turning " + myState + " " + myDeviceName)
      else:
        url = 'http://'+ conf['secret']['haipaddress'] + ':' + conf['secret']['haport'] + '/api/states/' + myDeviceId
-       header = {'x-ha-access': conf['secret']['haapipassword'], 'Content-Type': 'application/json'}
        response = get(url, headers=header)
        hermes.publish_end_session(current_session_id, myDeviceName + " is " + response.json()['state'])
     except:
